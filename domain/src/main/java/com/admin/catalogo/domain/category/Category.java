fa63ea25609453be05b0fdfe567be8c3d65a9545
Jacqueline Angelin <jacqueline.angelin@yahoo.com.br>
2022-08-18 17:33:00 -0300

    add initial project

---
+++ domain/src/main/java/com/admin/catalogo/domain/category/Category.java
@@ -0,0 +1,8 @@
+package com.admin.catalogo.domain.category;
+
+public class Category {
+    public String id;
+
+    public static void newCategory(String expectedName, String expectedDescription, boolean expectedIsActive) {
+    }
+}
---
+++ infrastructure/src/test/java/MainTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    void testMain() {
+        Assertions.assertNotNull(new Main());
+        Main.main(new String[]{});
+    }
+}
